d9fddc3c534c0bf730762e7e4de57eb45cd637c7
ATruglio <andreatrugliot72001@gmail.com>
2019-02-02 12:59:15 +0000

    Aggiustare errori

--- maggiore2.c
+++ maggiore2.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 int main(){
-int a,b;
+int a,b,max;
 printf("Inserisci un numero \n");
 scanf("%d",&a);
 printf("Inserisci un secondo numero \n");
 scanf("%d",&b);
+max=a;
 if(a<b)
-	a=b;
-printf("Il numero maggiore tra i due e \n %d",a);
+	max=b;
+printf("Il numero maggiore tra i due e \n %d",max);
 return 0;
 }
--- maggiore5.c
+++ maggiore5.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(){
-int a,b,c,d,e;
+int a,b,c,d,e,max;
 printf("Inserisci un numero \n");
 scanf("%d",&a);
 printf("Inserisci un secondo numero \n");
@@ -11,14 +11,15 @@ printf("Inserisci un quarto numero \n");
 scanf("%d",&d);
 printf("Inserisci un quinto numero \n");
 scanf("%d",&e);
-if (a<b)
-	a=b;
-if(a<c)
-	a=c;
-if(a<d)
-	a=d;
-if(a<e)
-	a=e;
+max=a;
+if (max<b)
+	max=b;
+if(max<c)
+	max=c;
+if(max<d)
+	max=d;
+if(max<e)
+	max=e;
 printf("Il numero maggiore e \n %d",a);
 return 0;
 }
